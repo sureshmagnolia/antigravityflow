6bbf43b1f4d0497186aa342257e0a9e2e4cbac85
sureshmagnolia <118750471+sureshmagnolia@users.noreply.github.com>
2025-11-19 00:10:10 +0530

    Update app.js

--- scr4/app.js
+++ scr4/app.js
@@ -1049,8 +1049,8 @@ generateDaywiseReportButton.addEventListener('click', async () => {
         
         let allPagesHtml = '';
         let totalPagesGenerated = 0;
-        const STUDENTS_PER_COLUMN = 30; 
-        const COLUMNS_PER_PAGE = 2; 
+        const STUDENTS_PER_COLUMN = 45; 
+        const COLUMNS_PER_PAGE = 1; 
         const STUDENTS_PER_PAGE = STUDENTS_PER_COLUMN * COLUMNS_PER_PAGE; 
 
         function buildColumnTable(studentChunk) {
